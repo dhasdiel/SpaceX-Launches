a7166d2e61a63804aa44cbd31591371ce4c10ce8
dhasdiel <danielhasdiel@gmail.com>
2022-05-24 16:36:20 +0300

    Finish SpaceX Launches App

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const cors = require("cors");
 const schema = require("./schema");
+const path = require("path");
 
 const app = express();
+
+app.use(cors());
 
 app.use(
   "/graphql",
@@ -12,5 +16,12 @@ app.use(
   })
 );
 
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
